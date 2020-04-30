609dc4c31f3a4de8f8e234eea8ceca7c9a7aaa0b
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-04-30 11:09:01 -0700

    Initial daily commit| Partial functionality in the index.js

--- index.js
+++ index.js
@@ -25,17 +25,15 @@ function starterQ() {
   inquirer.prompt(questions).then(function (answers) {
     console.log(answers);
     if (answers.employeeType === "Add Departments") {
-      console.log("Ask Manager questions");
       departmentAdd();
     } else if (answers.employeeType === "View Departments") {
-      console.log("Ask Intern Questions");
       departmentView();
     } else if (answers.employeeType === "Delete Departments") {
-      console.log("Ask Engineer Questions");
       departmentDelete();
     } else if (answers.employeeType === "Add Roles") {
-      console.log("Ask Employee Questions");
       rolesAdd();
+    } else if (answers.employeeType === "View Roles") {
+      rolesView();
     }
   });
 }
@@ -63,30 +61,132 @@ function departmentView() {
   connection.query(query, function (err, result) {
     console.log(err);
     console.table(result);
+    starterQ();
   });
 }
 
 function departmentDelete() {
   var query = "SELECT * FROM department";
   connection.query(query, function (err, result) {
-    var deptArray = [];
+    // var deptArray = [];
+    var deptArrayDisplay = [];
     for (var i = 0; i < result.length; i++) {
-      deptArray.push(result[i].name);
+      deptArrayDisplay.push("ID:" + result[i].id + " " + result[i].name);
+      // deptArray.push(result[i].name);
     }
     var questions = [
       {
         type: "list",
-        name: "Departments",
+        name: "departments",
         message: "What would you like to do?",
-        choices: deptArray,
+        choices: deptArrayDisplay,
       },
     ];
     inquirer.prompt(questions).then(function (answers) {
       console.log(answers);
-      var query = "DELETE FROM department WHERE NAME = answers.Departments";
-      connection.query(query, [answers.department], function (err, results) {
+      console.log(result);
+      var str = answers.departments;
+      var matches = str.match(/(\d+)/);
+      console.log(matches[0]);
+      var query = "DELETE FROM department WHERE id = " + matches[0];
+      connection.query(query, function (err, results) {
         console.log(err, results);
+        connection.query("SELECT * FROM department", function (errOne, resOne) {
+          if (errOne) throw errOne;
+          const transformed = resOne.reduce((acc, { id, ...x }) => {
+            acc[id] = x;
+            return acc;
+          }, {});
+          console.table(transformed);
+        });
+        var questions = [
+          {
+            type: "list",
+            name: "finishDelete",
+            message: "What would you like to do?",
+            choices: ["Main Menu", "Delete Another"],
+          },
+        ];
+        inquirer.prompt(questions).then(function (answers) {
+          if (answers.finishDelete === "Delete Another") {
+            departmentDelete();
+          } else {
+            starterQ();
+          }
+        });
       });
     });
+  });
+}
+
+function rolesAdd() {
+  var query = "SELECT * FROM department";
+  connection.query(query, function (err, result) {
+    // var deptArray = [];
+    var deptArrayDisplay = [];
+    for (var i = 0; i < result.length; i++) {
+      deptArrayDisplay.push(result[i].name);
+      // deptArray.push(result[i].name);
+    }
+    console.table(deptArrayDisplay);
+
+    var questions = [
+      {
+        type: "list",
+        name: "department",
+        message: "To which department would you like to add a role?",
+        choices: deptArrayDisplay,
+      },
+      {
+        type: "input",
+        name: "jobTitle",
+        message: "What is the Title of the role?",
+      },
+      {
+        type: "number",
+        name: "salary",
+        message: "What is the base pay of this role?",
+      },
+    ];
+    inquirer.prompt(questions).then(function (answers) {
+      console.log(answers);
+      var department_id;
+      for (var i = 0; i < result.length; i++) {
+        if (result[i].name === answers.department) {
+          department_id = result[i].id;
+        }
+      }
+
+      var query = "INSERT INTO roles (title, salary, department_id) values (?,?,?)";
+      connection.query(query, [answers.jobTitle, answers.salary, department_id], function (err, results) {
+        console.log(err);
+        console.table(results);
+
+        var questions = [
+          {
+            type: "list",
+            name: "finishRoles",
+            message: "What would you like to do?",
+            choices: ["Main Menu", "View Roles"],
+          },
+        ];
+        inquirer.prompt(questions).then(function (answers) {
+          if (answers.finishRoles === "View Roles") {
+            rolesView();
+          } else {
+            starterQ();
+          }
+        });
+      });
+    });
+  });
+}
+
+function rolesView() {
+  var query = "SELECT * FROM roles";
+  connection.query(query, function (err, result) {
+    console.log(err);
+    console.table(result);
+    starterQ();
   });
 }
